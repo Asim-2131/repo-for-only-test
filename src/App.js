464256eff2ce26243a7a1ac17e87ce5e019dcd5b
Asim Vora <asimvora2001@gmail.com>
2023-05-30 23:10:48 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const PushNotification = () => {
+  useEffect(() => {
+    const showNotification = () => {
+      if (Notification.permission === 'granted') {
+        // Create a new notification
+        const notification = new Notification('Hello!', {
+          body: 'This is a push notification!',
+        });
+      }
+    };
+
+    const requestPermission = () => {
+      // Request permission to display notifications
+      if (Notification.permission !== 'granted') {
+        Notification.requestPermission().then((permission) => {
+          if (permission === 'granted') {
+            // Permission granted, show the notification
+            showNotification();
+          }
+        });
+      } else {
+        // Permission already granted, show the notification
+        showNotification();
+      }
+    };
+
+    // Set the specific time to show the notification (e.g., 10 seconds from now)
+    const notificationTime = new Date().getTime() + 5000;
+
+    // Calculate the delay until the specific time
+    const delay = notificationTime - new Date().getTime();
+
+    // Schedule the notification
+    const timeout = setTimeout(requestPermission, delay);
+
+    return () => {
+      // Clear the timeout on component unmount
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return <div>Push Notification Example</div>;
+};
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My App</h1>
+      <PushNotification />
     </div>
   );
-}
+};
 
 export default App;
